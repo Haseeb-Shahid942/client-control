43aee0b5d3699897ca0e0c190fc7106ec750b26c
TechSol <tsol90085@gmail.com>
2026-06-22 16:34:28 +0500

    Update safety.js

--- safety.js
+++ safety.js
@@ -1,20 +1,20 @@
 async function safetyChecker(siteName) {
     try {
-        let security_key = "true";
         let url = `https://raw.githubusercontent.com/Haseeb-Shahid942/client-control/main/clients/${siteName}.txt?t=${Date.now()}`;
 
         const response = await fetch(url);
 
-        if (!response.ok) return;
+        if (!response.ok) {
+            console.log("File not found");
+            return;
+        }
 
-        let customer_security_key = (await response.text()).trim().toLowerCase();
+        let status = (await response.text()).trim().toLowerCase();
 
-        if (security_key === customer_security_key) {
-            console.log("YES");
-        } else {
-            document.body.innerHTML = '';
+        if (status !== "true") {
+            document.body.innerHTML = "";
         }
     } catch (error) {
-        console.error("Request failed:", error);
+        console.error(error);
     }
 }
